054f3c63c85427956b425b30a81b45660589bc9b
arun <arundarsh62@gmail.com>
2024-11-26 12:22:45 +0530

    second push

---
+++ Stringmethod/src/stringmethod/String_Replace.java
@@ -0,0 +1,13 @@
+package stringmethod;
+
+public class String_Replace {
+
+	public static void main(String[] args) {
+
+		String s1 = " hello world";
+		String s2 = s1.replace("l", "#");
+		System.out.println(s2);
+
+	}
+
+}
