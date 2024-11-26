f1d9ddfcef06a065ec78ec80157dd6f28d34e0f7
arun <arundarsh62@gmail.com>
2024-11-26 12:20:24 +0530

    second push

---
+++ Stringmethod/src/stringmethod/Stringappend.java
@@ -0,0 +1,12 @@
+package stringmethod;
+
+public class Stringappend {
+
+	public static void main(String[] args) {
+		StringBuilder s1=new StringBuilder("how are");
+		s1.append(" you");
+		System.out.println(s1);
+
+	}
+
+}
